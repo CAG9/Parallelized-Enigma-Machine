e7e0a264d7f8d347183f5acce67e636f04944bb6
Ladivcr <vrosas832@gmail.com>
2021-06-13 01:24:32 -0500

    Aun tiene errores pero mañana espero poder subir la primer función de las tres que me tocan

--- V_Enigma.c
+++ V_Enigma.c
@@ -2,16 +2,27 @@
 //#include <mpi.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-int main(){
-    // Reflectores: Tres posibles
-    // Reflector A, Reflector B, Reflector C
-    char* reflectors [] = {"EJMZALYXVBWFCRQUONTSPIKHGD",     
+
+// short %d Tipo entero 
+// int %d Tipo entero 
+// long int %ld Tipo entero 
+// long long int %Ld Tipo entero
+// float %f Coma flotante de baja precisión 
+// double %f Coma flotante de mayor precisión
+// long double %Lf Coma flotante de mayor precisión 
+// char %c ó %s Caracter(es)
+
+
+// Reflectores: Tres posibles
+// Reflector A, Reflector B, Reflector C
+char* reflectors [] = {"EJMZALYXVBWFCRQUONTSPIKHGD",     
         "YRUHQSLDPXNGOKMIEBFZCWVJAT",        
         "FVPJIAOYEDRZXWGCTKUQSBNMHL"}; 
 
-    // Rotores: Ocho posibles
-    char* con_rotors [] = {"EKMFLGDQVZNTOWYHXUSPAIBRCJ",        
+// Rotores: Ocho posibles
+char* con_rotors [] = {"EKMFLGDQVZNTOWYHXUSPAIBRCJ",        
         "AJDKSIRUXBLHWTMCQGZNPYFVOE",        
         "BDFHJLCPRTXVZNYEIWGAKMUSQO",        
         "ESOVPZJAYQUIRHXLNFTGKDCMWB",        
@@ -20,8 +31,8 @@ int main(){
         "NZJHGRCXMYSWBOUFAIVLPEKQDT",        
         "FKQHTLXOCBJSPDZRAMEWNIUYGV"};       
 
-    // Rotores de retorno: Ocho posibles
-    char* rotors_return [] = {"UWYGADFPVZBECKMTHXSLRINQOJ",
+// Rotores de retorno: Ocho posibles
+char* rotors_return [] = {"UWYGADFPVZBECKMTHXSLRINQOJ",
         "AJPCZWRLFBDKOTYUQGENHXMIVS",
         "TAGBPCSDQEUFVNZHYIXJWLRKOM",
         "HZWVARTNLGUPXQCEJMBSKDYOIF",
@@ -30,17 +41,37 @@ int main(){
         "QMGYVPEDRCWTIANUXFKZOSLHJB",
         "QJINSAYDVKBFRUHMCPLEWZTGXO"};
 
-    // Alfabeto
-    char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// Alfabeto
+char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    // short %d Tipo entero 
-    // int %d Tipo entero 
-    // long int %ld Tipo entero 
-    // long long int %Ld Tipo entero
-    // float %f Coma flotante de baja precisión 
-    // double %f Coma flotante de mayor precisión
-    // long double %Lf Coma flotante de mayor precisión 
-    // char %c ó %s Caracter(es)
+
+
+int ordc(char character[]){
+    
+    int i= (int)character[0];
+    return i;
+    
+}
+
+int char_to_pos(char txt[]){
+    int pos = ordc(txt)-65;
+    return pos;
+    }
+
+void set_reflector(struct enigmaM3 self_reflector, char txt[]){
+	/*The function set_reflector from pythonenigma.py
+	 * parameters: 
+	 * self_reflector type: enigmaM3
+	 * txt type: char
+     * -> self_reflector corresponds to the parameter
+     * reflector from our class enigmaM3*/
+    char self_reflector; 
+	self_reflector = reflectors[char_to_pos(txt)];
+    return self_reflector;
+    }
+
+
+int main(){
 
     // esto es el class enigmaM3 de .py
     typedef struct enigmaM3{
@@ -56,5 +87,21 @@ int main(){
 
     }enigmaM3;
     
+    char reflector[1] = {"B"}; 
+    int rotors[3] = {4, 1, 3}; 
+    char plugboard[6][2] = {{'A','M'},{'F','I'},{'N','V'},{'P','S'},{'T','U'},{'W','Z'}};
+    char pos_ini[3] = {"A", "B", "C"}; 
+    char intern_position[3] = {"A", "A", "A"}; 
+    char notch [8][2] = {{'Q','\0'},{'E','\0'},{'V','\0'},{'J','\0'},{'Z','\0'},{'Z','M'},{'Z','M'},{'Z','M'}};
+    char opts[5] = {rotors, reflector, pos_ini, intern_position, plugboard};
+
+    struct enigmaM3 myMachine = {reflector, rotors, plugboard,
+                        pos_ini, intern_position, notch, opts};
+
+    printf(myMachine.rotors);
+    
     return 0;
 }
+
+
+

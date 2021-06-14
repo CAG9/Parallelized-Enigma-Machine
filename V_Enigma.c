f587cd09b54cbc1641e5e5bd24a61d26f4756d46
Ladivcr <vrosas832@gmail.com>
2021-06-14 00:09:53 -0500

    Primer función: 40 %

--- V_Enigma.c
+++ V_Enigma.c
@@ -45,6 +45,51 @@ char* rotors_return [] = {"UWYGADFPVZBECKMTHXSLRINQOJ",
 char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 
+int main(){
+
+    // esto es el class enigmaM3 de .py
+    typedef struct enigmaM3{
+        char *reflector[1]; // Es una letra: "B"
+        /*(Es tres porque en el código python a lo más que llega es a 2 pero el otro
+        espacio en el arreglo es para \0 "espacio vacio por defeto" ) */
+        int *rotors[3]; // Es una tupla: (4, 1, 3)
+        char *plugboard[6][2]; // Arreglo de tuplas: [('A','M'),('F','I'),('N','V'),('P','S'),('T','U'),('W','Z')]
+        char *pos_ini[3]; // Posición inicio se actualiza con cada caracter 
+        char *intern_position[3]; // VALOR INICIAL = {"A", "A", "A"};
+        char *notch[8][2]; // VALOR INICIAL = (('Q',),('E',),('V',),('J',),('Z',),('Z','M'),('Z','M'),('Z','M'));
+        char *opts[5]; // Guardar la configuración inicial
+
+    }enigmaM3;
+    
+    char *reflector[1] = {'B'}; 
+    int *rotors[3] = {4, 1, 3}; 
+    char *plugboard[6][2] = {{'A','M'},{'F','I'},{'N','V'},{'P','S'},{'T','U'},{'W','Z'}};
+    char *pos_ini[3] = {'A', 'B', 'C'}; 
+    char *intern_position[3] = {'A', 'A', 'A'}; 
+    char *notch[8][2] = { {'Q', '\0'}, {'E', '\0'}, {'V', '\0'}, {'J', '\0'}, {'Z', '\0'}, {'Z', 'M'}, {'Z', 'M'}, {'Z', 'M'} };
+    char *opts[5] = {rotors, reflector, pos_ini, intern_position, plugboard};
+
+    struct enigmaM3 myMachine = {&reflector, &rotors, &plugboard,
+                        &pos_ini, &intern_position, &notch, &opts};
+
+    printf("reflector: %s\n", &reflector[0]);
+    printf("rotors: %d\n", rotors[1]);
+    printf("plugboard: %s\n", &plugboard[5][0]);
+    printf("pos_ini: %s\n", &pos_ini[1]);
+    printf("intern_position: %s\n", &intern_position[1]);
+    printf("notch: %s%s\n", &notch[6][0], &notch[6][1]);
+    printf("opts: %s\n", &opts[1][0]);
+    printf("------------------------------------\n");
+    printf("-----------------EnigmaM3-----------\n");
+    printf("Reflector M3: %s\n", myMachine.reflector[0]);
+    printf("Rotors M3: %ls\n", myMachine.rotors[0]);
+    
+    return 0;
+}
+
+
+/*
+
 
 int ordc(char character[]){
     
@@ -59,49 +104,15 @@ int char_to_pos(char txt[]){
     }
 
 void set_reflector(struct enigmaM3 self_reflector, char txt[]){
-	/*The function set_reflector from pythonenigma.py
-	 * parameters: 
-	 * self_reflector type: enigmaM3
-	 * txt type: char
-     * -> self_reflector corresponds to the parameter
-     * reflector from our class enigmaM3*/
-    char self_reflector; 
+	//The function set_reflector from pythonenigma.py
+	// * parameters: 
+	// * self_reflector type: enigmaM3
+	// * txt type: char
+    // * -> self_reflector corresponds to the parameter
+    //  * reflector from our class enigmaM3
+    struc enigmaM3 self_reflector; 
 	self_reflector = reflectors[char_to_pos(txt)];
     return self_reflector;
     }
 
-
-int main(){
-
-    // esto es el class enigmaM3 de .py
-    typedef struct enigmaM3{
-        char reflector[1]; // Es una letra: "B"
-        /*(Es tres porque en el código python a lo más que llega es a 2 pero el otro
-        espacio en el arreglo es para \0 "espacio vacio por defeto" ) */
-        int rotors [3]; // Es una tupla: (4, 1, 3)
-        char plugboard[6][2]; // Arreglo de tuplas: [('A','M'),('F','I'),('N','V'),('P','S'),('T','U'),('W','Z')]
-        char pos_ini[3]; // Posición inicio se actualiza con cada caracter 
-        char intern_position[3]; // VALOR INICIAL = {"A", "A", "A"};
-        char notch[8][2]; // VALOR INICIAL = (('Q',),('E',),('V',),('J',),('Z',),('Z','M'),('Z','M'),('Z','M'));
-        char opts[5]; // Guardar la configuración inicial
-
-    }enigmaM3;
-    
-    char reflector[1] = {"B"}; 
-    int rotors[3] = {4, 1, 3}; 
-    char plugboard[6][2] = {{'A','M'},{'F','I'},{'N','V'},{'P','S'},{'T','U'},{'W','Z'}};
-    char pos_ini[3] = {"A", "B", "C"}; 
-    char intern_position[3] = {"A", "A", "A"}; 
-    char notch [8][2] = {{'Q','\0'},{'E','\0'},{'V','\0'},{'J','\0'},{'Z','\0'},{'Z','M'},{'Z','M'},{'Z','M'}};
-    char opts[5] = {rotors, reflector, pos_ini, intern_position, plugboard};
-
-    struct enigmaM3 myMachine = {reflector, rotors, plugboard,
-                        pos_ini, intern_position, notch, opts};
-
-    printf(myMachine.rotors);
-    
-    return 0;
-}
-
-
-
+*/
